04ef2f5f98c57d798559a6e74cae8019a3ab500d
Rehab Tasawar <111608024+rehabcoding@users.noreply.github.com>
2025-03-22 17:46:38 +0000

    msg

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 1403
+let accountEmail = "rehabtasawar@gmail.com"
+var accountPassword = "123456"
+accountCity = "Islamabad"
+let accountState
+
+// accountId = 2
+
+accountEmail = "rr"
+accountPassword = "2"
+accountCity = "Lahore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
