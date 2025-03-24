d13732e1bd2c8d5d68568dd261b25b5a6aad311a
Rehab Tasawar <111608024+rehabcoding@users.noreply.github.com>
2025-03-24 15:10:04 +0000

    learning

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -5,3 +5,22 @@
 console.log(3+3)
 
 console.log(typeof "Rehab")
+
+//Stack and Heap Memory
+
+let myName = "rehab"
+
+let anotherName = myName
+
+anotherName = "rehab tasawar"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = 
+{
+    email: "rt@gmail.com",
+    id: "010101011"
+}
+
+let userTwo = userOne
---
+++ basics/strings.js
@@ -0,0 +1,13 @@
+const name = "rehab"
+const repoCount = 2
+
+// console.log(name + repoCount + " Value");  // outdated syntax
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Rehab-T')
+
+// console.log(gameName.indexOf('j'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
