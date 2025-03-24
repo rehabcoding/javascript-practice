0cd4231abb5131936bd73fe0eb795b5d96178878
Rehab Tasawar <111608024+rehabcoding@users.noreply.github.com>
2025-03-24 16:11:12 +0000

    Numbers and Maths

---
+++ basics/numbersMath.js
@@ -0,0 +1,11 @@
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString())
+
+// console.log(Math.round(4.6))
+ console.log(Math.floor(Math.random()*6) + 1)
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
